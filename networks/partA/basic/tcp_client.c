07ec8901dad46e44b04fb2ea67a5022ca0a44c27
Amey Karan <ameykaran2005@gmail.com>
2023-09-25 22:06:58 +0530

    Completed tcp and udp client-server programs

--- networks/partA/basic/tcp_client.c
+++ networks/partA/basic/tcp_client.c
@@ -4,7 +4,15 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-int main(){
+#define printerror(func, msg) \
+    if (func < 0)             \
+    {                         \
+        perror("[-]" msg);    \
+        exit(1);              \
+    }
+
+int main()
+{
     char *ip = "127.0.0.1";
     int port = 5500;
 
@@ -15,10 +23,8 @@ int main(){
     int n;
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0){
-        perror("[-]Socket error");
-        exit(1);
-    }
+    printerror(sock, "Socket error");
+
     printf("[+]TCP server socket created.\n");
 
     memset(&addr, '\0', sizeof(addr));
@@ -26,21 +32,20 @@ int main(){
     addr.sin_port = port;
     addr.sin_addr.s_addr = inet_addr(ip);
 
-    connect(sock, (struct sockaddr*)&addr, sizeof(addr));
+    printerror(connect(sock, (struct sockaddr *)&addr, sizeof(addr)), "Connection error");
     printf("Connected to the server.\n");
 
     bzero(buffer, 1024);
-    strcpy(buffer, "HELLO, THIS IS CLIENT.");
+    strcpy(buffer, "I am the client.");
     printf("Client: %s\n", buffer);
-    send(sock, buffer, strlen(buffer), 0);
+    printerror(send(sock, buffer, strlen(buffer), 0), "Send to server");
 
     bzero(buffer, 1024);
-    recv(sock, buffer, sizeof(buffer), 0);
+    printerror(recv(sock, buffer, sizeof(buffer), 0), "Receiving from server");
     printf("Server: %s\n", buffer);
 
     close(sock);
     printf("Disconnected from the server.\n");
 
     return 0;
-
 }
--- networks/partA/basic/tcp_server.c
+++ networks/partA/basic/tcp_server.c
@@ -4,21 +4,26 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-int main() {
+#define printerror(func, msg) \
+    if (func < 0)             \
+    {                         \
+        perror("[-]" msg);    \
+        exit(1);              \
+    }
+
+int main()
+{
     char *ip = "127.0.0.1";
     int port = 5500;
 
-    int server_sock, client1_sock, client2_sock;
-    struct sockaddr_in server_addr, client1_addr, client2_addr;
+    int server_sock, client_sock;
+    struct sockaddr_in server_addr, client_addr;
     socklen_t addr_size;
     char buffer[1024];
     int n;
 
     server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock < 0) {
-        perror("[-]Socket error");
-        exit(1);
-    }
+    printerror(server_sock, "Socket error");
     printf("[+]TCP server socket created.\n");
 
     memset(&server_addr, '\0', sizeof(server_addr));
@@ -26,34 +31,29 @@ int main() {
     server_addr.sin_port = port;
     server_addr.sin_addr.s_addr = inet_addr(ip);
 
-    n = bind(server_sock, (struct sockaddr *) &server_addr, sizeof(server_addr));
-    if (n < 0) {
-        perror("[-]Bind error");
-        exit(1);
-    }
+    n = bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    printerror(n, "Bind error");
     printf("[+]Binded to the port number: %d\n", port);
 
-    listen(server_sock, 5);
+    printerror(listen(server_sock, 5), "Listen");
     printf("Listening...\n");
 
-    while (1) {
-        addr_size = sizeof(client1_addr);
-        client1_sock = accept(server_sock, (struct sockaddr *) &client1_addr, &addr_size);
-        printf("[+]Client 1 connected.\n");
-        client2_sock = accept(server_sock, (struct sockaddr *) &client2_addr, &addr_size);
-        printf("[+]Client 2 connected.\n");
+    addr_size = sizeof(client_addr);
+    client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &addr_size);
+    printerror(client_sock, "Accept");
+    printf("[+]Client connected.\n");
 
-        bzero(buffer, 1024);
-        recv(client1_sock, buffer, sizeof(buffer), 0);
-        printf("Client: %s\n", buffer);
+    bzero(buffer, 1024);
+    printerror(recv(client_sock, buffer, sizeof(buffer), 0), "Receiving from the client");
+    printf("Client: %s\n", buffer);
 
-        bzero(buffer, 1024);
-        strcpy(buffer, "HI, THIS IS SERVER. HAVE A NICE DAY!!!");
-        printf("Server: %s\n", buffer);
-        send(client1_sock, buffer, strlen(buffer), 0);
+    bzero(buffer, 1024);
+    strcpy(buffer, "Hello from the server!");
+    printf("Server: %s\n", buffer);
+    printerror(send(client_sock, buffer, strlen(buffer), 0), "Sending to the client");
 
-
-    }
+    close(client_sock);
+    printf("[+]Client disconnected.\n\n");
 
     return 0;
 }
